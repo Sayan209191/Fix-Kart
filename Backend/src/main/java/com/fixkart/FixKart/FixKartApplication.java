a538d3ff02d6bb0a9c2e991c552f35f822df59d2
sayan209191 <sayanghorui611@gmail.com>
2025-08-12 23:40:58 +0530

    Initial commit

---
+++ Backend/src/main/java/com/fixkart/FixKart/FixKartApplication.java
@@ -0,0 +1,13 @@
+package com.fixkart.FixKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixKartApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/fixkart/FixKart/FixKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixkart.FixKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
