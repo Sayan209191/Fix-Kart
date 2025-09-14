9be4a143b881d8e43b9560c7f28b76196ac2ead8
chinmaykhanra <chinmaykhanra302@gmail.com>
2025-09-14 10:43:07 +0530

    Basic Frontend Added:

---
+++ frontend/src/main.server.ts
@@ -0,0 +1,8 @@
+import 'zone.js/node';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config.server';
+import { App } from './app/app';
+
+const bootstrap = () => bootstrapApplication(App, appConfig);
+
+export default bootstrap;
